84ee70edf8ad5ecf5afbff0c89ba835983cd06f6
Nishaam-1019941 <113018089+Nishaam-1019941@users.noreply.github.com>
2024-10-03 14:16:49 +0200

    hashed password voor users

--- save_students.py
+++ save_students.py
@@ -1,16 +1,11 @@
 import json
 import sqlite3
 
-
 with open('students.json', 'r') as f:
     data = json.load(f)
 
-
 conn = sqlite3.connect('students.db')
-
-
 c = conn.cursor()
-
 
 for student in data:
     student_class = student['student_class']
@@ -21,8 +16,5 @@ for student in data:
     c.execute("INSERT INTO students (class, name, student_id) VALUES (?, ?, ?)",
               (student_class, student_name, student_number))
 
-
 conn.commit()
-
-
 conn.close()
--- save_users.py
+++ save_users.py
@@ -1,16 +1,22 @@
 import sqlite3
+import hashlib
 
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
 
 conn = sqlite3.connect('students.db')
-
-
 c = conn.cursor()
 
-# insert a user into db
+hashed_password = hash_password('admin')
 c.execute("INSERT INTO users (username, password, name, isAdmin) VALUES (?, ?, ?, ?)",
-          ('admin', 'admin', 'admin', 1)) 
+          ('admin', hashed_password, 'admin', 1))
+
+c.execute("SELECT username, password FROM users")
+users = c.fetchall()
+
+for username, password in users:
+    hashed_password = hash_password(password)
+    c.execute("UPDATE users SET password = ? WHERE username = ?", (hashed_password, username))
 
 conn.commit()
-
-
 conn.close()
